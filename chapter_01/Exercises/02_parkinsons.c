875de8b89b43a6a4e70dabc4b3193343ddeb825d
Marcel24991 <marcelabend249@gmail.com>
2020-08-02 00:51:44 -0400

    Adding Exercises

---
+++ chapter_01/Exercises/02_parkinsons.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // -> directive
+
+int main(void)
+{
+	printf("Parkinson's Law: \nWork expands so as to "); // -> statement 1
+	printf("fill the time\n"); // -> statement 2
+	printf("available for its completion. \n"); // -> statement 3
+	return 0; // -> statement 4
+}
