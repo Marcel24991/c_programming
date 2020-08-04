b9624f7cc2b33ebb649847fe73d6a86fdef3663f
Marcel24991 <marcelabend249@gmail.com>
2020-08-04 01:12:28 -0400

    Moved to folder

---
+++ chapter_03/03_Exercises/03_05_scanf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	float x,y;
+
+	scanf("%f%d%f", &x, &i, &y);
+
+	printf("x -> %f\n", x); //12.3
+	printf("i -> %d\n", i); // 45
+	printf("y -> %f\n", y); // 0.6
+	return 0;
+}
